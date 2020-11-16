60a3b116ec8752f1b9de19497fa3fa0414f61909
Ishan Dutta <ishandutta2007@gmail.com>
2020-11-16 17:42:22 +0530

    minor

--- add_numbers_data_creation.py
+++ add_numbers_data_creation.py
@@ -1,12 +1,28 @@
-import numpy as np 
-train_data = np.array([[1.0,1.0]])
+import numpy as np
+import random
+
+train_data = np.array([[1.0, 1.0]])
 train_targets = np.array([2.0])
+for i in range(3, 10000, 2):
+    s = random.randint(i, 3 * i)
+    i2 = s - i
+    train_data = np.append(train_data, [[i, i2]], axis=0)
+    train_targets = np.append(train_targets, [s])
+
+print("train_data")
 print(train_data)
-for i in range(3,10000,2):
-    train_data= np.append(train_data,[[i,i]],axis=0)
-    train_targets= np.append(train_targets,[i+i])
-test_data = np.array([[2.0,2.0]])
+print("train_targets")
+print(train_targets)
+
+test_data = np.array([[2.0, 2.0]])
 test_targets = np.array([4.0])
-for i in range(4,8000,4):
-    test_data = np.append(test_data,[[i,i]],axis=0)
-    test_targets = np.append(test_targets,[i+i])
+for i in range(4, 8000, 4):
+    s = random.randint(i, 2 * i)
+    i2 = s - i
+    test_data = np.append(test_data, [[i, i2]], axis=0)
+    test_targets = np.append(test_targets, [s])
+
+print("test_data")
+print(test_data)
+print("test_targets")
+print(test_targets)
--- add_numbers_model_and_predict.py
+++ add_numbers_model_and_predict.py
@@ -1,22 +1,27 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import data_creation as dc 
+import add_numbers_data_creation as dc
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(2,)),
-    keras.layers.Dense(20, activation=tf.nn.relu),
-	keras.layers.Dense(20, activation=tf.nn.relu),
-    keras.layers.Dense(1)
-])
+print("dc.train_data:")
+print(dc.train_data)
+print("dc.train_targets:")
+print(dc.train_targets)
 
-model.compile(optimizer='adam', 
-              loss='mse',
-              metrics=['mae'])
+model = keras.Sequential(
+    [
+        keras.layers.Flatten(input_shape=(2,)),
+        keras.layers.Dense(20, activation=tf.nn.relu),
+        keras.layers.Dense(20, activation=tf.nn.relu),
+        keras.layers.Dense(1),
+    ]
+)
 
-model.fit(dc.train_data, dc.train_targets, epochs=10, batch_size=1)
+model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+
+model.fit(dc.train_data, dc.train_targets, epochs=50, batch_size=1)
 
 test_loss, test_acc = model.evaluate(dc.test_data, dc.test_targets)
-print('Test accuracy:', test_acc)
-a= np.array([[2000,3000],[4,5]])
+print("Test accuracy:", test_acc)
+a = np.array([[2000, 2000], [5, 5], [15, 25], [2345, 4535], [24245, 5]])
 print(model.predict(a))
